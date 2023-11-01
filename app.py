d7282ced88f5f81c89c1ad98a213c2f99cc73650
RonaldPurba <ronalpurba912@gmail.com>
2023-11-01 09:52:06 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
-import requests
-from bs4 import BeautifulSoup
-
 import os
+import requests
 from os.path import join, dirname
 from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+from bs4 import BeautifulSoup
 
 MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
+
 db = client[DB_NAME]
 
 app = Flask(__name__)
